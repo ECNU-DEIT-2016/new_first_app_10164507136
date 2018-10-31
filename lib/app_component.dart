61e8d9c7ed6a4b40ee71f473db005d08ffc7a16f
qingfeng1016 <1204145685@qq.com>
2018-10-31 22:28:39 +0800

    第一次作业补交

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,8 @@
 import 'package:angular/angular.dart';
 
 import 'src/todo_list/todo_list_component.dart';
+import 'src/hero_form/hero_form_component.dart';
+
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -8,8 +10,10 @@ import 'src/todo_list/todo_list_component.dart';
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
+ // template: '<hero-form></hero-form>',
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [TodoListComponent,
+  HeroFormComponent],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
